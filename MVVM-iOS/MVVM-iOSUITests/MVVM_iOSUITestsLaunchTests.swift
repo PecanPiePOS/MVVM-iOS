c647c9f05dbe060a9bb3c2e24a5a79146032eea1
PecanPie <hidra0321@naver.com>
2023-08-02 16:58:53 +0900

    :tada: Created a project for MVVM Studying

---
+++ MVVM-iOS/MVVM-iOSUITests/MVVM_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVM_iOSUITestsLaunchTests.swift
+//  MVVM-iOSUITests
+//
+//  Created by KYUBO A. SHIM on 2023/08/02.
+//
+
+import XCTest
+
+final class MVVM_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
