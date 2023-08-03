3258d6b0a3b9f6956fa64701dcbf7851255fd6fd
PecanPie <hidra0321@naver.com>
2023-08-03 20:08:44 +0900

    Added Input protocol for VM

--- MVVM-iOS/MyLibrary/ViewModels/PhotoAlbumViewModel.swift
+++ MVVM-iOS/MyLibrary/ViewModels/PhotoAlbumViewModel.swift
@@ -7,4 +7,34 @@
 
 import UIKit
 
+public protocol PhotoAlbumViewModelInputs {
+    /// Call when the indivisual cell is tapped.
+    func photoCellTapped()
+    
+    /// Call when the images are updated.
+    func photosUpdated()
+    
+    /// Call when the view should be refreshed.
+    func refresh()
+    
+    /// Call when the button that animates cells is tapped.
+    func animateCellButtonTapped()
+    
+    /// Call when the view did load.
+    func viewDidLoad()
+    
+    /// Call when the view will appear with animated property.
+    func viewWillAppear(animated: Bool)
+}
 
+public protocol PhotoAlbumViewModelOutputs {
+    
+}
+
+public protocol PhotoAlbumViewModelType {
+    
+}
+
+public final class PhotoAlbumViewModel: PhotoAlbumViewModelType, PhotoAlbumViewModelInputs, PhotoAlbumViewModelOutputs {
+    
+}
