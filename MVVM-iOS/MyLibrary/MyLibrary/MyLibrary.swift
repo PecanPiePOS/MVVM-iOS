6e792d90bd3d5fc75965b116ae72dbdf757aa8be
PecanPie <hidra0321@naver.com>
2023-08-03 16:06:08 +0900

    Made an example Code for using Library

--- MVVM-iOS/MyLibrary/MyLibrary/MyLibrary.swift
+++ MVVM-iOS/MyLibrary/MyLibrary/MyLibrary.swift
@@ -5,6 +5,11 @@
 //  Created by KYUBO A. SHIM on 2023/08/03.
 //
 
-class MyLibrary {
-
+public class MyLibraryTest {
+    
+    public init() {}
+    
+    public func multiplyAndMinus(with base: Int, times: Int, minus: Int) -> Int {
+        return base * times - minus
+    }
 }
