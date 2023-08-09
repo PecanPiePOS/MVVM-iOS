38c5f009a1be29fac20461d40c023c6e870a2524
PecanPie <hidra0321@naver.com>
2023-08-09 23:58:43 +0900

    TestClass from framework - test class

--- MyFramework/MyFramework/TestFramework.swift
+++ MyFramework/MyFramework/TestFramework.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+public class FrameworkTestClass {
+    public private(set) var embedValue: Int = 0
+    
+    public func setRandomEmbedValue(as value: Int) {
+        let randomValueForCalculating = [3,6,7]
+        self.embedValue = value * (randomValueForCalculating.randomElement() ?? 1)
+    }
+}
