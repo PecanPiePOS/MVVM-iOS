7a730758df00f7bfb40b9ee320cac72a8702b0ff
PecanPie <hidra0321@naver.com>
2023-08-10 02:36:32 +0900

    public init is Required. Do not forget

--- MyFramework/MyFramework/TestFramework.swift
+++ MyFramework/MyFramework/TestFramework.swift
@@ -7,8 +7,15 @@
 
 import Foundation
 
+import RxSwift
+import RxRelay
+
 public class FrameworkTestClass {
     public private(set) var embedValue: Int = 0
+    public var disposeBag = DisposeBag()
+    public var testObservable: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+    
+    public init() {}
     
     public func setRandomEmbedValue(as value: Int) {
         let randomValueForCalculating = [3,6,7]
