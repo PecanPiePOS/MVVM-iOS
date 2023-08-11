ce69c779ed16076183e874e9cf1a31dcd24febb8
PecanPie <hidra0321@naver.com>
2023-08-12 01:48:22 +0900

    Added files for Model

---
+++ MyFramework/Activity.swift
@@ -0,0 +1,8 @@
+//
+//  Activity.swift
+//  MyFramework
+//
+//  Created by KYUBO A. SHIM on 2023/08/12.
+//
+
+import Foundation
---
+++ MyFramework/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoModel.swift
+//  MyFramework
+//
+//  Created by KYUBO A. SHIM on 2023/08/12.
+//
+
+import UIKit
+
+public struct PhotoModel {
+    let imageName: String
+    let takenPlace: String
+    let takenTime: Date
+    let imageDataSize: Double
+    let shutterSpeed: String
+}
