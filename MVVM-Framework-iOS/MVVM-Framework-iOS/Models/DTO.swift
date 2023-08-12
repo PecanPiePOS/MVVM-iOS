9b5b13b3ce7d7740fd623a2549f227f94cf07a4a
PecanPie <hidra0321@naver.com>
2023-08-12 23:43:22 +0900

    Added files for View and Model to the project file

---
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Models/DTO.swift
@@ -0,0 +1,8 @@
+//
+//  DTO.swift
+//  MVVM-Framework-iOS
+//
+//  Created by KYUBO A. SHIM on 2023/08/12.
+//
+
+import Foundation
---
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Views/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  MVVM-Framework-iOS
+//
+//  Created by KYUBO A. SHIM on 2023/08/12.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
