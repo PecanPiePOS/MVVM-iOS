643e90416315f3b07724ec092836d8f898ec26ce
PecanPie <hidra0321@naver.com>
2023-08-13 20:15:13 +0900

    Updated all UI using RxSwift

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Controllers/HomeViewController.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Controllers/HomeViewController.swift
@@ -6,24 +6,108 @@
 //
 
 import UIKit
+import MyFramework
 
-class HomeViewController: UIViewController {
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
 
+final class HomeViewController: UIViewController {
+
+    private var disposeBag = DisposeBag()
+    private let viewModel: PhotoAlbumViewModelType = PhotoAlbumViewModel()
+    
+    private let refreshButton = UIButton(type: .system)
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: setFlowLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bindStyles()
+        bindViewModel()
+        bindLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    deinit {
+        print("HomeVC OUT")
     }
-    */
+}
 
+extension HomeViewController {
+    
+    private func bindViewModel() {
+        self.viewModel.outputs.photoAlbumLists
+            .bind(to: collectionView.rx.items(cellIdentifier: "photoCell", cellType: PhotoCell.self))
+            { row, element, cell in
+                cell.configure(imageOf: element)
+            }
+            .disposed(by: disposeBag)
+        
+        self.refreshButton.rx.tap
+            .bind {
+                self.viewModel.inputs.refresh()
+            }
+            .disposed(by: disposeBag)
+        
+        
+        self.collectionView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindStyles() {
+        view.backgroundColor = .orange
+        
+        refreshButton.do {
+            $0.setImage(UIImage(systemName: "arrow.clockwise.circle.fill"), for: .normal)
+            $0.tintColor = .white
+        }
+        
+        collectionView.do {
+            $0.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+            $0.isScrollEnabled = false
+            $0.backgroundColor = .clear
+            $0.contentInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+        }
+    }
+    
+    private func bindLayout() {
+        view.addSubview(collectionView)
+        view.addSubview(refreshButton)
+        
+        refreshButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(50)
+            $0.bottom.equalToSuperview().inset(100)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(1.5)
+        }
+    }
+    
+    private func setFlowLayout() -> UICollectionViewFlowLayout {
+        let flowlayout = UICollectionViewFlowLayout()
+        flowlayout.minimumLineSpacing = 15
+        flowlayout.minimumInteritemSpacing = 15
+        return flowlayout
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = view.frame.width / 2.5
+        let height: CGFloat = view.frame.height / 4
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+        self.viewModel.inputs.updatePhotos(index: indexPath.item)
+    }
+}
+
+extension HomeViewController {
+    
 }
