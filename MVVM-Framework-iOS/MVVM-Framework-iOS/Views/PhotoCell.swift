6aa059bd02690ad66195c580737da5eacb1a03cd
PecanPie <hidra0321@naver.com>
2023-08-13 20:12:36 +0900

    Completed CollectionView Cell

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Views/PhotoCell.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Views/PhotoCell.swift
@@ -7,6 +7,54 @@
 
 import UIKit
 
-class PhotoCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class PhotoCell: UICollectionViewCell {
     
+    private let photoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bindStyles()
+        bindLayout()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.photoImageView.image = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PhotoCell {
+    private func bindStyles() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 16
+        self.layer.masksToBounds = true
+        
+        self.photoImageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.layer.masksToBounds = true
+            $0.isUserInteractionEnabled = true
+        }
+    }
+    
+    private func bindLayout() {
+        self.addSubview(photoImageView)
+        
+        photoImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension PhotoCell {
+    func configure(imageOf image: String) {
+        self.photoImageView.image = UIImage(named: image)
+    }
 }
--- MVVM-Framework-iOS/MVVM-Framework-iOS/Views/PhotoCellCircleView.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Views/PhotoCellCircleView.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
-class PhotoCellCircleView: UIView {
+final class PhotoCellCircleView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .yellow
     }
-    */
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension PhotoCellCircleView {
+    private func bindStyles() {}
+    private func bindLayout() {}
 }
