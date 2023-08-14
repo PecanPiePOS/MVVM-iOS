90ecde714e557cc30e321588cbbb1440e8d9fb40
PecanPie <hidra0321@naver.com>
2023-08-14 23:14:28 +0900

    Added Caution using SKProductsRequestDelegate with Thread Issues

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
@@ -46,6 +46,11 @@ final class IAPServie: NSObject {
     }
 }
 
+    /**
+     `SKProductsRequestDelegate` 주의할 점:
+     - Warning
+     - Responses received by the SKProductsRequestDelegate may not be returned on a specific thread. If you make assumptions about which queue will handle delegate responses, you may encounter unintended performance and compatibility issues in the future.
+     */
 extension IAPServie: SKProductsRequestDelegate {
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
         if !response.products.isEmpty {
