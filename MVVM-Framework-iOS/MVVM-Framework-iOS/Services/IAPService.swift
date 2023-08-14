4ad8a7c319650f5324f7edb458540e7be39dc5f5
PecanPie <hidra0321@naver.com>
2023-08-14 18:54:30 +0900

    Added IAP Product struct to get product Ids

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import StoreKit
+
+    /**
+     Sets Ids and Store Information of In-App products.
+     - Contains Ids
+     */
+struct IAPProducts {
+    typealias ConsumableId = String
+    typealias SubsriptionId = String
+    
+    /// #1 Consumable IAP product Identifier
+    static let consumableProduct1: ConsumableId = "com.pecanpie.test.MALI"
+    
+    /// #1 Subscription IAP product Identifier
+    static let subscriptionProduct1: SubsriptionId = "com.pecanpie.test.MALI.substription"
+    
+    private static let consumableProductIdentifiers: Set<ConsumableId> = [IAPProducts.consumableProduct1]
+    private static let subscriptionProductIdentifiers: Set<SubsriptionId> = [IAPProducts.subscriptionProduct1]
+    
+//    static let comsumableStore = IAP
+}
+
