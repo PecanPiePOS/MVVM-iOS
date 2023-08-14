3cc3716c4865e4f572085b24b7b96fd3b130e6dd
PecanPie <hidra0321@naver.com>
2023-08-14 23:15:38 +0900

    Added Purchase VC

---
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Controllers/PurchaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PurchaseViewController.swift
+//  MVVM-Framework-iOS
+//
+//  Created by KYUBO A. SHIM on 2023/08/14.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
