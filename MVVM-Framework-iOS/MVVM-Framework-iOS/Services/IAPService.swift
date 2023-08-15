62a334245eed274501649a57caea433a1c6a97d4
PecanPie <hidra0321@naver.com>
2023-08-15 18:41:24 +0900

    Added methods for purchasing products.

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Services/IAPService.swift
@@ -8,8 +8,9 @@
 import Foundation
 import StoreKit
 
-public typealias ConsumableId = String
-public typealias SubsriptionId = String
+typealias ConsumableId = String
+typealias SubsriptionId = String
+typealias ProductRequestCompletionHandler = (_ success: Bool, _ products: [SKProduct]?) -> Void
 
     /**
      Sets Ids and Store Information of In-App products.
@@ -28,37 +29,68 @@ struct IAPProducts {
 //    static let comsumableStore = IAP
 }
 
+    /// Will be joining Framework, and be using RxSwift afterwards.
 final class IAPServie: NSObject {
+    
     /// 앱스토어에서 Localized 정보를 가져온다.
     /// - Strong Reference 를 유지해야 한다. 그 이유는 request 가 완료되기 전에 메모리에서 해제되지 않게 하기 위함이다.
     private var request: SKProductsRequest!
-    
     private var products: [SKProduct] = []
+    private var productsRequestCompletionHandler: ProductRequestCompletionHandler?
+    // 일단 받아오는 값을 계속 봐보자.
+    private var productIds: [String]
     
-    /// 지금 이 Ids 에서 Subscription 과 Consumable 이 나눠져 내려오는지 궁금하다.
-    func request(ids: [String]) {
-        let ids: Set<String> = Set(ids)
-        request = SKProductsRequest(productIdentifiers: ids)
-        
+    init(productIds: [String]) {
+        self.productIds = productIds
+        super.init()
+        // SkPaymentTransactionObserver 를 채택하면 된다.
+        SKPaymentQueue.default().add(self)
+    }
+    
+    /// App Store Connect 에서 등록한 인앱 결제 상품들을 가져온다.
+    func requestProducts(handler completionHadler: @escaping ProductRequestCompletionHandler) {
+        let idSet = Set(productIds)
+        request.cancel()
+        request = SKProductsRequest(productIdentifiers: idSet)
         // Response 를 SKProductsRequestDelegate 를 사용하기 위함이다
         request.delegate = self
         request.start()
+    }
+    
+    /// 인앱 상품을 구입한다.
+    func buyProduct(product: SKProduct) {
+        let payment = SKPayment(product: product)
+        // SKPaymentQueue: A queue of payment transactions to be processed by the App Store.
+        SKPaymentQueue.default().add(payment)
+    }
+    
+    /// 가장 최신의 구입 내역을 복원한다.
+    func restorePreviousPurchase() {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    
+    /// 해당 유저의 모든 구입 내역을 복원한다.
+    func restoreAllPurchases(of user: String?) {
+        SKPaymentQueue.default().restoreCompletedTransactions(withApplicationUsername: user)
     }
 }
 
     /**
      `SKProductsRequestDelegate` 주의할 점:
-     - Warning
+     - [Warning]
      - Responses received by the SKProductsRequestDelegate may not be returned on a specific thread. If you make assumptions about which queue will handle delegate responses, you may encounter unintended performance and compatibility issues in the future.
      */
 extension IAPServie: SKProductsRequestDelegate {
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
         if !response.products.isEmpty {
-            self.products = response.products
+            let products = response.products
+            // 왜? 이걸 여기에 담고, 바로 clear 를 하면 무슨 의미가 없지?
+            productsRequestCompletionHandler?(true, products)
+            clearRequestAndHandler()
         }
         
         for invalidId in response.invalidProductIdentifiers {
-            /**
+            /*
              🚩 InvalidProductIdentifiers 핸들링 방법 -
              
              - invalidProductIdentifiers란? App Store Connect에서 상품 구성을 잘못한 경우 발생
@@ -67,4 +99,38 @@ extension IAPServie: SKProductsRequestDelegate {
              */
         }
     }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        productsRequestCompletionHandler?(false, nil)
+    }
+    
+    private func clearRequestAndHandler() {
+        request = nil
+        productsRequestCompletionHandler = nil
+    }
+}
+
+extension IAPServie: SKPaymentTransactionObserver {
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+    }
+}
+
+extension IAPServie {
+    
+}
+
+extension IAPServie {
+    
+}
+
+extension IAPServie {
+    
+}
+
+extension IAPServie {
+    
+}
+
+extension IAPServie {
+    
 }
