8af13a92407e52151e1c63f1bd9ffc7e9819f158
PecanPie <hidra0321@naver.com>
2023-08-19 16:22:22 +0900

    Added Codable Extension

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Util/Encodable+.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Util/Encodable+.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Encodable {
+    
+    public func asParameter() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                as? [String: Any] else {
+            throw NSError()
+        }
+        return dictionary
+    }
+}
