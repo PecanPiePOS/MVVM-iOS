e6f35490366e51e3fde7bb507418225bea8a5e9d
PecanPie <hidra0321@naver.com>
2023-08-20 21:22:23 +0900

    Testing out few codes of RxMoya

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Controllers/MoyaRxTestViewController.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Controllers/MoyaRxTestViewController.swift
@@ -9,19 +9,60 @@ import UIKit
 import MyFramework
 
 import Moya
+import RxCocoa
+import RxMoya
+import RxSwift
 import SnapKit
 import Then
 
 final class MoyaRxTestViewController: UIViewController {
 
+//    private let provider = MoyaProvider<NetworkApi>(plugins: [NetworkLoggerPlugin(verbose: true)])
     private let provider = MoyaProvider<NetworkApi>()
-    
+    private var disposeBag = DisposeBag()
     private let tableView = UITableView(frame: .zero)
     private let nameLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setViewModel()
+        setStyles()
+        setLayout()
+    }
+    
+    private func setViewModel() {
+        let userFetchData = UserPageModel(page: 2, perPage: 3)
+        
+        // ⭐️⭐️⭐️ 왜 Moya 를 Rx 로 써야하는지... 참고: https://leejigun.github.io/Tayga-Moya,RxSwift
+        let requestFetchingList =  provider.rx.request(.fetchListOfUsers(userFetchData)).asObservable()
+        
+        
+//        requestFetchingList.subscribe { event in
+//            switch event {
+//            case .success(let response):
+//                guard let data = try? response.map(GeneralResponse<[UserData]>.self).data else { return }
+//                print("⭐️⭐️⭐️⭐️⭐️⭐️")
+//                print(data)
+//                print("⭐️⭐️⭐️⭐️⭐️⭐️")
+//
+//                /// 1. 여기서 받은걸 Observable 로 만들기
+//                /// 2. Framework 에 넣을 수 없는 이유
+//                ///     a. Computed Property 를 사용하면, Dynamic Framework 에서는 Build 오류가 발생
+//                ///  - https://hururuek-chapchap.tistory.com/230
+//
+//            case .failure(_):
+//                print("⚠️⚠️⚠️⚠️")
+//            }
+//        }
+//        .disposed(by: self.disposeBag)
+    }
+    
+    private func setStyles() {
         view.backgroundColor = .blue
+
+    }
+    
+    private func setLayout() {
+        
     }
 }
