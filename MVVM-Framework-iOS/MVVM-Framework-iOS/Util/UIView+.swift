68e9b95bb9931263cf8afc8abdd08998fddbb334
PecanPie <hidra0321@naver.com>
2023-09-16 23:55:18 +0900

    Added UIView Extension

--- MVVM-Framework-iOS/MVVM-Framework-iOS/Util/UIView+.swift
+++ MVVM-Framework-iOS/MVVM-Framework-iOS/Util/UIView+.swift
@@ -5,4 +5,29 @@
 //  Created by KYUBO A. SHIM on 2023/09/04.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func scaleView(by scale: CGFloat) {
+        self.contentScaleFactor = scale
+        for subview in self.subviews {
+            subview.scaleView(by: scale)
+        }
+    }
+    
+    func saveUIViewWithScale(with scale: CGFloat? = nil) -> Data? {
+        let newScale = scale ?? UIScreen.main.scale
+        self.scaleView(by: newScale)
+        
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = newScale
+        let renderer = UIGraphicsImageRenderer(bounds: bounds, format: format)
+        let image = renderer.image { context in
+            self.layer.render(in: context.cgContext)
+        }
+        let imageData = image.pngData()
+        
+        return imageData
+    }
+}
